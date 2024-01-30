2526cfca3d7d8133d5b0f71c41a8e93980d7b33a
Kemsekov <pikova98@bk.ru>
2024-01-30 17:13:31 +0500

    continue writing syntax analysis

--- compiler/Modules/SyntaxAnalysis.cs
+++ compiler/Modules/SyntaxAnalysis.cs
@@ -1,9 +1,10 @@
 namespace Modules;
+using static Lexical;
 public class SyntaxAnalysis
 {
-    public SyntaxAnalysis(LexicalAnalysis lexical,InputOutput inputOutput,ErrorDescriptions errorDescriptions, ConfigurationVariables configuration)
+    public SyntaxAnalysis(LexicalAnalysis lexical, InputOutput inputOutput, ErrorDescriptions errorDescriptions, ConfigurationVariables configuration)
     {
-        Configuration = configuration;  
+        Configuration = configuration;
         LexicalAnalysis = lexical;
         ErrorDescriptions = errorDescriptions;
         InputOutput = inputOutput;
@@ -13,26 +14,115 @@ public class SyntaxAnalysis
     public LexicalAnalysis LexicalAnalysis { get; }
     public ErrorDescriptions ErrorDescriptions { get; }
     public InputOutput InputOutput { get; }
-
+    byte Symbol => LexicalAnalysis.Symbol;
+    void type()
+    {
+        //TODO:
+        // check current symbol to be valid type, 
+        //for now just accept ident
+        Accept(ident);
+    }
+    void SimpleType()
+    {
+        // check that current symbol is valid simple type (int,float,string)
+        if (LexicalAnalysis.SymbolValue == "int" || LexicalAnalysis.SymbolValue == "float" || LexicalAnalysis.SymbolValue == "string")
+        {
+            Accept(ident);
+            return;
+        }
+        InputOutput.LineErrors().Add(
+            new Error
+            {
+                ErrorCode = (long)ErrorCodes.UnexpectedSymbol,
+                Position = LexicalAnalysis.Pos,
+                SpecificErrorDescription = $"Expected type int,float or string, but {LexicalAnalysis.SymbolValue} found"
+            }
+        );
+    }
+    void variable()
+    {
+        Accept(ident);
+        while (Symbol == lbracket || Symbol == point
+        || Symbol == arrow)
+            switch (Symbol)
+            {
+                case lbracket:
+                    LexicalAnalysis.NextSym(); Expression();
+                    while (Symbol == comma)
+                    {
+                        LexicalAnalysis.NextSym(); Expression();
+                    }
+                    Accept(rbracket);
+                    break;
+                case point:
+                    LexicalAnalysis.NextSym(); Accept(ident);
+                    break;
+                case arrow:
+                    LexicalAnalysis.NextSym();
+                    break;
+            }
+    }
+    void VarDeclaration()
+    {
+        Accept(varsy);
+        while (Symbol == comma)
+        {
+            LexicalAnalysis.NextSym();
+            Accept(ident);
+        }
+        Accept(colon);
+        type();
+    }
+    void VarPart()
+    {
+        if (Symbol == varsy)
+        {
+            Accept(varsy);
+            do
+            {
+                VarDeclaration();
+                Accept(semicolon);
+            }
+            while (Symbol == ident);
+        };
+    }
+    void ArrayType()
+    {
+        Accept(arraysy);
+        Accept(lbracket);
+        SimpleType();
+        while (Symbol == comma)
+        {
+            LexicalAnalysis.NextSym();
+            SimpleType();
+        }
+        Accept(rbracket);
+        Accept(ofsy);
+        type();
+    }
     /// <summary>
     /// Accepts current symbol if it is equal to <paramref name="expectedSymbol"/> and moves to next symbol
     /// </summary>
     /// <returns>True if symbol is accepted.</returns>
-    bool Accept(byte expectedSymbol){
-        if(LexicalAnalysis.Symbol==expectedSymbol){
+    bool Accept(byte expectedSymbol)
+    {
+        if (Symbol == expectedSymbol)
+        {
             LexicalAnalysis.NextSym();
             return true;
         }
-        else{
+        else
+        {
             InputOutput.LineErrors().Add(
-                new Error{
-                    ErrorCode= (long)ErrorCodes.UnexpectedSymbol,
-                    Position=LexicalAnalysis.Pos,
-                    SpecificErrorDescription=$"Expected {Keywords.InverseKw[expectedSymbol]}"
+                new Error
+                {
+                    ErrorCode = (long)ErrorCodes.UnexpectedSymbol,
+                    Position = LexicalAnalysis.Pos,
+                    SpecificErrorDescription = $"Expected {Keywords.InverseKw[expectedSymbol]}"
                 }
             );
             return false;
         }
     }
-    
+
 }
